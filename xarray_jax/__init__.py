f65d74e47e4c9350482785c014168470c03c4269
Allen Wang <allenw@mit.edu>
2024-09-25 22:41:43 -0400

    Add var_change_on_unflatten to public API

--- xarray_jax/__init__.py
+++ xarray_jax/__init__.py
@@ -1,6 +1,7 @@
 import xarray
 from xarray_jax.custom_types import XjDataArray, XjDataset, XjVariable, from_xarray, to_xarray
+from xarray_jax.register_pytrees import var_change_on_unflatten
 
 xarray.set_options(keep_attrs=True)  # Necessary for preserving PyTree structure.
 
-__all__ = ["XjDataArray", "XjDataset", "XjVariable", "from_xarray", "to_xarray"]
+__all__ = ["XjDataArray", "XjDataset", "XjVariable", "from_xarray", "to_xarray", "var_change_on_unflatten"]
