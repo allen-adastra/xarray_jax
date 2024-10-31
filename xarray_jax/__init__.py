6860e39694664eea302af25bb796e968cd154361
Allen Wang <allenw@mit.edu>
2024-10-31 12:26:07 -0400

    Autoformat

--- xarray_jax/__init__.py
+++ xarray_jax/__init__.py
@@ -1,7 +1,20 @@
 import xarray
-from xarray_jax.custom_types import XjDataArray, XjDataset, XjVariable, from_xarray, to_xarray
+from xarray_jax.custom_types import (
+    XjDataArray,
+    XjDataset,
+    XjVariable,
+    from_xarray,
+    to_xarray,
+)
 from xarray_jax.register_pytrees import var_change_on_unflatten
 
 xarray.set_options(keep_attrs=True)  # Necessary for preserving PyTree structure.
 
-__all__ = ["XjDataArray", "XjDataset", "XjVariable", "from_xarray", "to_xarray", "var_change_on_unflatten"]
+__all__ = [
+    "XjDataArray",
+    "XjDataset",
+    "XjVariable",
+    "from_xarray",
+    "to_xarray",
+    "var_change_on_unflatten",
+]
